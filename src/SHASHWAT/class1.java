545be681a22e2a6fc93a61691bda916fbcf0c009
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:31:04 +0530

    Update class1.java

--- src/SHASHWAT/class1.java
+++ src/SHASHWAT/class1.java
@@ -1,4 +1,17 @@
 package SHASHWAT;
 
+import java.util.Scanner;
+
 public class class1 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("what is your name : ");
+        String name=sc.nextLine();
+        System.out.println("hi "+name);
+        System.out.println("enter the age : ");
+        String age=sc.next();
+        int myage=Integer.parseInt(age);
+        System.out.println("your age is "+myage);
+
+    }
 }
