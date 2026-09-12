0d860983a7fd7ea13da19b425707bc9397771289
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:41 +0530

    Update class3.java

--- src/SHASHWAT/class3.java
+++ src/SHASHWAT/class3.java
@@ -1,4 +1,32 @@
 package SHASHWAT;
 
 public class class3 {
+    static void main() {
+     /*   String name=new String("xyz");
+      //  String name2=name;
+      //  System.out.println(name==name2);
+     //   name2="abc";
+     //   System.out.println(name==name2);
+        String name3=new String("xyz");
+        String name4=new String("xyz");
+        String name5=new String("xyz");
+        String name6=new String("xyz");
+        System.out.println(name3.equals(name4));
+        System.out.println(name4.equals(name6));
+        System.out.println(name6.equals(name5));
+        System.out.println(name.equals(name3));
+
+        String name="xyz";
+        String name1="xyz";
+        String name2="xyz";
+        String name3="xyz";
+        System.out.println(name3==name1);
+        System.out.println(name1==name2);
+        System.out.println(name==name3);
+        System.out.println(name2==name3);
+
+*/
+
+
+    }
 }
