b71586a6f30dbed2c81b3ce6039f6be735cf9866
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:49 +0530

    Update class2.java

--- src/SHASHWAT/class2.java
+++ src/SHASHWAT/class2.java
@@ -1,4 +1,14 @@
 package SHASHWAT;
 
 public class class2 {
+    static void main() {
+        String n1="brajul";
+        System.out.println(n1);
+        String n2=n1;
+        System.out.println(n1);
+        n2="look";
+        System.out.println(n2);
+
+
+    }
 }
