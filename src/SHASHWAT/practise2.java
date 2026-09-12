4ed3420df5a2f4ec04678d3e4af7d3fec76ee883
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:33 +0530

    Update practise2.java

--- src/SHASHWAT/practise2.java
+++ src/SHASHWAT/practise2.java
@@ -1,4 +1,12 @@
 package SHASHWAT;
 
 public class practise2 {
+    static void main() {
+        String location="pune, hyderabad,noida ,bangalore";
+        //conert to String aray
+        String  ch[]=location.split(",");
+       for(int i=0;i<ch.length;i++){
+           System.out.print(ch[i]+" ");
+       }
+    }
 }
