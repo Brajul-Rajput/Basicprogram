2186eb597cdf4338dca1d621c4e2d1af098c3d57
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:00 +0530

    Update addnum.java

--- src/BASIC/addnum.java
+++ src/BASIC/addnum.java
@@ -1,4 +1,17 @@
 package BASIC;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
 public class addnum {
+    static void main() {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the first digit : ");
+        int num1=sc.nextInt();
+        System.out.println("enter the second digit : ");
+        int num2=sc.nextInt();
+        int sum=num1+num2;
+        System.out.println("sum is : "+sum);
+    }
 }
