9ed1ed1b10638bb298b729e5433bff5a71b20f50
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:36 +0530

    Update class5.java

--- src/SHASHWAT/class5.java
+++ src/SHASHWAT/class5.java
@@ -1,4 +1,25 @@
 package SHASHWAT;
 
 public class class5 {
+    static void main() {
+        String name="Brajul rajput";
+     //   System.out.println(name.toLowerCase());
+      //  System.out.println(name);
+      //  System.out.println(name.toUpperCase());
+      //  System.out.println(name);
+        //System.out.println(name.charAt(5));
+      //  System.out.println(name.indexOf('j'));
+     //   System.out.println(name.length());
+      /*
+        char ch[]=name.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            System.out.print(ch[i]+" ");
+        }
+         */
+
+       String name2="brajulrajput";
+        System.out.println(name.compareTo(name2));
+
+
+    }
 }
