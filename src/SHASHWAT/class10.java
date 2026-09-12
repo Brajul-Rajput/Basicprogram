3286b67f6afc04e1a52c2e9d21d9c458c7ca7ba8
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:55 +0530

    Update class10.java

--- src/SHASHWAT/class10.java
+++ src/SHASHWAT/class10.java
@@ -1,4 +1,17 @@
 package SHASHWAT;
 
+import java.util.Scanner;
+
 public class class10 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the digit : ");
+        int num=sc.nextInt();
+        if(num%2!=0){
+            System.out.println("number is odd "+num);
+        }
+        else{
+            System.out.println("number is even "+num);
+        }
+    }
 }
