74843d01a648454962141095bd4f5f096e18d583
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:31 +0530

    Update class8.java

--- src/SHASHWAT/class8.java
+++ src/SHASHWAT/class8.java
@@ -1,4 +1,16 @@
 package SHASHWAT;
 
 public class class8 {
+    static void main() {
+        int num=1234;
+        int first=8;
+        int count=0;
+        while(num>0){
+            num=num/10;
+            count++;
+        }
+        int ans=first*(int)Math.pow(10,count);
+        int f=ans+num;
+        System.out.println(f);
+    }
 }
