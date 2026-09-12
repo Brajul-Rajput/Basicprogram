b6a29903a046ad404eacc741583eed6e2320e557
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:27 +0530

    Update practise3.java

--- src/SHASHWAT/practise3.java
+++ src/SHASHWAT/practise3.java
@@ -1,4 +1,15 @@
 package SHASHWAT;
 
 public class practise3 {
+    static void main() {
+        String email="brajulrajput@gmail.com";
+        String email2="brajulrajput@hotmail.com";
+     //   System.out.println(email.split("@"));
+        String ch[]=email.split("@");
+        for(int i=0;i< ch.length;i++){
+            System.out.print(ch[i]+"");
+        }
+       // System.out.println(email2.split("@"));
+
+    }
 }
