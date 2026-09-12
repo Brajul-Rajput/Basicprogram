b3b11ee97c52b8336c4030b787114b0ca0f033e1
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:40 +0530

    Update class9.java

--- src/SHASHWAT/class9.java
+++ src/SHASHWAT/class9.java
@@ -1,4 +1,20 @@
 package SHASHWAT;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class class9 {
+    static void main() {
+        //reverse the number
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the digit : ");
+        int num=sc.nextInt();
+        int rev=0;
+        while(num>0){
+            int unit=num%10;
+            rev=rev*10+unit;
+            num=num/10;
+        }
+        System.out.println("after reverse "+rev);
+    }
 }
