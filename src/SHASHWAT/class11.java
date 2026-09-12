d460cd25e78d63d888ef22ae4bc7649f3ad7f980
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:43 +0530

    Update class11.java

--- src/SHASHWAT/class11.java
+++ src/SHASHWAT/class11.java
@@ -1,4 +1,17 @@
 package SHASHWAT;
 
+import java.util.Scanner;
+
 public class class11 {
+    static void main() {
+        // find the power 2^4=16
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the digit  : ");
+        int num=sc.nextInt();
+        System.out.println("enter the power of the digit : ");
+        int pow=sc.nextInt();
+        int res=(int)Math.pow(num,pow);
+        System.out.println(res);
+
+    }
 }
