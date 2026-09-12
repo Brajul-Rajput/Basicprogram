8ee63517d2a899fd7b3bf2226bf38f3169d58789
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:29 +0530

    Update practise.java

--- src/SHASHWAT/practise.java
+++ src/SHASHWAT/practise.java
@@ -1,4 +1,13 @@
 package SHASHWAT;
 
 public class practise {
+    static void main() {
+        int op=15;
+        int ans=op++ + ++op - --op -op++;
+      //  System.out.println(op+"  "+ans);
+        int b=5;
+        int x=(27+40)-b++*5/2+9%100;
+        System.out.println(x);
+
+    }
 }
