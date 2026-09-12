e32e8ea3fc136f48c4e9f6c0b5f94d1e359ff854
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:46 +0530

    Update class6.java

--- src/SHASHWAT/class6.java
+++ src/SHASHWAT/class6.java
@@ -1,4 +1,21 @@
 package SHASHWAT;
 
+import java.util.Scanner;
+
 public class class6 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number : ");
+        int num=sc.nextInt();
+        System.out.println("number is "+num+" and digit count is "+countdigt(num));
+
+    }
+    static int countdigt(int num){
+        int count=0;
+        while(num>0){
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
 }
