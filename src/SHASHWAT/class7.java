3e3d18369e0df23afdee1ebfc5f7f69c801bbc40
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:35 +0530

    Update class7.java

--- src/SHASHWAT/class7.java
+++ src/SHASHWAT/class7.java
@@ -1,4 +1,23 @@
 package SHASHWAT;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class class7 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the digit : ");
+        int num=sc.nextInt();
+        System.out.println("enter the append digit : ");
+        int num2=sc.nextInt();
+    //    appenddigit(num,num2);
+    }
+    //not used for negitive number
+    /*
+    static void appenddigit(int num ,int num2){
+        int add=num*10+num2;
+        System.out.println(add);
+    }
+
+     */
 }
