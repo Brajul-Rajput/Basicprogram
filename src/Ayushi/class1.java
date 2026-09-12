fbb5c646e077bd5008f8d27efe0a111265ff2305
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:29:57 +0530

    Update class1.java

--- src/Ayushi/class1.java
+++ src/Ayushi/class1.java
@@ -1,4 +1,14 @@
 package Ayushi;
 
+import java.util.Scanner;
+
 public class class1 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array :  ");
+        int size=sc.nextInt();
+      //  int arr[size]= int
+
+    }
+
 }
