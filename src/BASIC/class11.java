b37e065b3812c45f1ba82f888635c4316c173a7b
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:26:39 +0530

    Create class11.java

---
+++ src/BASIC/class11.java
@@ -0,0 +1,10 @@
+package BASIC;
+
+public class class11 {
+    static void main() {
+        // print the a to z
+        for(char ch='a';ch<='z';ch++){
+            System.out.print(ch+" ");
+        }
+    }
+}
