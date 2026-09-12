26a2aea8f2ee50c6823e699a40b9dfb3c9bb931e
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-12 17:30:38 +0530

    Update class4.java

--- src/SHASHWAT/class4.java
+++ src/SHASHWAT/class4.java
@@ -1,4 +1,26 @@
 package SHASHWAT;
 
+import java.util.Scanner;
+
 public class class4 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+      /*
+        System.out.println("enter the name : ");
+        String name=sc.nextLine();
+        System.out.println("enter the word :");
+        String word=sc.next();
+        System.out.println("name is "+name);
+        System.out.println("word is "+word);
+*/
+        System.out.println("enter the word :");
+        String word=sc.next();
+        sc.nextLine();
+        System.out.println("enter the name : ");
+        String name=sc.nextLine();
+
+        System.out.println("name is "+name);
+        System.out.println("word is "+word);
+
+    }
 }
