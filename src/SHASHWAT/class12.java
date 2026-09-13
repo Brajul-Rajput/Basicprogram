c254490db45ec01a9255dc0d97e78fe2a1c40961
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-13 21:51:17 +0530

    Create class12.java

---
+++ src/SHASHWAT/class12.java
@@ -0,0 +1,13 @@
+package SHASHWAT;
+
+public class class12 {
+    static void main() {
+        //decimal to anybase
+        int num=12;
+        int base =2;
+        while(num>0){
+            int rem=num%10;
+          //  rem
+        }
+    }
+}
