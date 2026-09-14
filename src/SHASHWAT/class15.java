5c4bd8dda180714074e8ab972f19ebf6330d3100
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-14 16:16:16 +0530

    Create class15.java

---
+++ src/SHASHWAT/class15.java
@@ -0,0 +1,13 @@
+package SHASHWAT;
+
+public class class15 {
+    static void main() {
+        class15 sm=new class15();
+        sm.newdigit(1238,4);
+    }
+    public void newdigit(int num,int num2){
+        int newdig=(num*10)+num2;
+        System.out.println(newdig);
+
+    }
+}
