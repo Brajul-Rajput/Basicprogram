04808e42d4a039e24295ab3699e86695d072c49e
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-14 21:46:21 +0530

    Create class46.java

---
+++ src/BASIC/class46.java
@@ -0,0 +1,14 @@
+package BASIC;
+
+public class class46 {
+    static void main() {
+        //pattern
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
