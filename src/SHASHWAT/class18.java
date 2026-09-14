2e74c956c55aa825b629334d96258444bd6d5acd
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-14 16:32:48 +0530

    Create class18.java

---
+++ src/SHASHWAT/class18.java
@@ -0,0 +1,15 @@
+package SHASHWAT;
+
+public class class18 {
+    static void main() {
+        oddeveb(45);
+    }
+    public static void oddeveb(int num){
+        if(num%2!=0){
+            System.out.println("odd");
+        }
+        else{
+            System.out.println("even");
+        }
+    }
+}
