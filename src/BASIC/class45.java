325cda01a5f0ea6b630806121a9b67f6f0ac3364
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-15 12:09:48 +0530

    Create class45.java

---
+++ src/BASIC/class45.java
@@ -0,0 +1,11 @@
+package BASIC;
+
+import java.util.Arrays;
+
+public class class45 {
+    static void main() {
+        int arr[]={12 ,34,21 ,9 ,3 ,34 ,65 ,87, 34 ,145};
+      Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
