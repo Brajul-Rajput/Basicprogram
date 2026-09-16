5e2e7796a7ee3bc2ef6da3955312303d367a45f8
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-16 16:01:32 +0530

    Create practise16.java

---
+++ src/SHASHWAT/practise16.java
@@ -0,0 +1,33 @@
+package SHASHWAT;
+
+public class practise16 {
+    static void main() {
+        int arr[]={2,4,5,7,7,7,7,10,11};
+        int target=7;
+        boolean fist=true;
+        int s=0;
+        int ans=-1;
+        int e=arr.length-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                if(fist){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else if(arr[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+}
+
