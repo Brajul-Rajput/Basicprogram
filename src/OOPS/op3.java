7a4ff9e5cf4129d5f6f99fe698bd5f7b3e10ce81
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-17 10:34:10 +0530

    Create op3.java

---
+++ src/OOPS/op3.java
@@ -0,0 +1,23 @@
+package OOPS;
+
+public class op3 {
+    static void main() {
+        n ok=new n(10,20,"ramu","kakm");
+        ok.sho();
+
+    }
+}
+
+class n{
+    n(int x,int y,String v,String g) {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(v);
+        System.out.println(g);
+    }
+        void sho(){
+            System.out.println("all print ");
+        }
+
+    }
+
