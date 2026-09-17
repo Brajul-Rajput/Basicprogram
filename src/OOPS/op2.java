0cbfb6efedffa9f357c19c5f7bd9fd606e55002e
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-17 10:34:08 +0530

    Create op2.java

---
+++ src/OOPS/op2.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class op2 {
+    static void main() {
+       A og=new A();
+       og.sh();
+    }
+}
+class A{
+    String name;
+    String  name2;
+    int age;
+    int age2;
+    void sh(){
+        System.out.println(name +" "+ name2+" "+age+" "+age2);
+    }
+}
