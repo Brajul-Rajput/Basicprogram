f50267ef29b813f6197a6480893f4f300c924ae1
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-17 10:34:05 +0530

    Create op1.java

---
+++ src/OOPS/op1.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class op1 {
+  static void main() {
+        demo obj =new demo();
+        obj.show();
+    }
+}
+class demo {
+    int a = 10;
+    int b = 2;
+    String name = "brajul";
+
+    void show() {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }
+}
