5b51c4f875d3bdfb569286a847cb6077d85dc45f
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-18 15:14:00 +0530

    Create p3.java

---
+++ src/Patterns/p3.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class p3 {
+    static void main() {
+        for(int i=1;i<5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
