f0bbb3fcfa09089fdd5c6a4f285b495cd84d6aef
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-18 15:14:26 +0530

    Create class30.java

---
+++ src/SHASHWAT/class30.java
@@ -0,0 +1,15 @@
+package SHASHWAT;
+
+public class class30 {
+    static void main() {
+        int arr[]={2,4,6,9,11,7,3,1};
+
+       for(int i=0;i<arr.length;i++) {
+           //if (arr[i] < arr[])
+               System.out.println(arr[i]+" yes");
+       }
+        System.out.println("not");
+
+    }
+
+}
