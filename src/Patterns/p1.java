1e4d380d09e250ac93b646edfb60e90f43c948e8
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-18 15:13:50 +0530

    Create p1.java

---
+++ src/Patterns/p1.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class p1 {
+    static void main() {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
