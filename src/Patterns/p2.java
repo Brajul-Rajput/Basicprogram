cc9caf43343a41fab971fcd92c89a72b3d67df1b
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-18 15:14:23 +0530

    Create p2.java

---
+++ src/Patterns/p2.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class p2 {
+    static void main() {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
