79c69772dcff533cd3b10404b8680db076b5acbd
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-18 15:14:05 +0530

    Create p4.java

---
+++ src/Patterns/p4.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class p4 {
+    static void main() {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print((char)65+j );
+            }
+            System.out.println("");
+        }
+    }
+}
