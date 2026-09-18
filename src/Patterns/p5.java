f99d05d361a98b8920b003e1b80574e9a01ff66f
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-18 15:14:08 +0530

    Create p5.java

---
+++ src/Patterns/p5.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class p5 {
+    static void main() {
+        for(int i=0;i<5;i++){
+            for(int j=4;j>i;j--){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
