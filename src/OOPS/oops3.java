e2c0681dc5265ff3801748a1af59bc39253ac081
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-19 12:00:52 +0530

    Create oops3.java

---
+++ src/OOPS/oops3.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class oops3 {
+    static void main() {
+c4 df=new c4();
+df.sh();
+    }
+}
+class c4{
+    String city;
+    boolean y;
+    void sh(){
+        System.out.println("are you live in "+city+" "+y);
+    }
+}
