f16ef6796195cd3a02b27078ece79087ee6567e0
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-19 12:01:31 +0530

    Create oops5.java

---
+++ src/OOPS/oops5.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class oops5 {
+    static void main() {
+     // c6 vd=new c6();
+    //  vd.ss();
+    }
+}
+class c6{
+    int a;
+    float b;
+    private c6(){
+        a=12;
+        b=123.45f;
+    }
+    void ss(){
+        System.out.println(a+" "+b);
+    }
+}
