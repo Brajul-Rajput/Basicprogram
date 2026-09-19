33b80f8f5fd85348c606ab5a8cd8ffe9f6dd8800
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-19 12:01:35 +0530

    Create oops9.java

---
+++ src/OOPS/oops9.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+public class oops9 {
+    static void main() {
+        c10 s=new c10();
+        s.sho();
+    }
+}
+class c10{
+    //instance block
+    {
+      //  int s=34;
+     //   System.out.println(s);
+        System.out.println("instancr block");
+    }
+    int c;
+    c10//int v){
+        (){
+      // c=v;
+      //  System.out.println(c);
+        System.out.println("default constuctor ");
+    }
+    void sho(){
+        System.out.println("method ");
+    }
+}
