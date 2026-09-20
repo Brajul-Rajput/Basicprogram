8fca6955f1331b42b2e5cfe303ecee23ffdfaea9
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-20 21:37:04 +0530

    Create leetcode67.java

---
+++ src/LEETCODE/leetcode67.java
@@ -0,0 +1,17 @@
+package LEETCODE;
+
+public class leetcode67 {
+    static void main() {
+        leetcode67 df=new leetcode67();
+      //  System.out.println( df.add("11","1"));
+
+    }
+    /*
+    public String add(String a ,String b){
+        int num=Integer.parseInt(a);
+        int num2=Integer.parseInt(b);
+       // return ;
+    }
+
+     */
+}
