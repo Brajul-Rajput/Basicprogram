d515c18e884994052ff62aed53b6a0eebd017269
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-23 17:00:10 +0530

    Create class34.java

---
+++ src/SHASHWAT/class34.java
@@ -0,0 +1,22 @@
+package SHASHWAT;
+
+public class class34 {
+    static void main() {
+        int arr[]={2,4,5,11,7,5,1};
+        findmin(arr);
+
+    }
+    public static void findmin(int arr[]){
+        //int ans=-1;
+        //for(int i=0;i< arr.length;i++){
+
+            if(arr[0]<arr[arr.length-1]){
+                System.out.println(arr[0]);;
+            }
+            else{
+                System.out.println(arr[arr.length-1]);
+            }
+        }
+
+
+}
