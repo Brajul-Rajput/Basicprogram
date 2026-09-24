a9865e7cbe2ad19dae9b9b8d9785d31f01205984
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-24 16:36:07 +0530

    Create class38.java

---
+++ src/SHASHWAT/class38.java
@@ -0,0 +1,16 @@
+package SHASHWAT;
+
+public class class38 {
+
+        static void main() {
+            int num=19;
+            if((num&1)==0){
+                System.out.println("even");
+            }
+            else{
+                System.out.println("odd");
+            }
+        }
+    }
+
+
