e81952d3d970b15e3443c11ef7d26e33c591495a
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-24 16:35:54 +0530

    Create oops15.java

---
+++ src/OOPS/oops15.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class oops15 {
+    static void main() {
+        c16 vv=new c16(122);
+        vv.ss();
+    }
+}
+class c16{
+    int a;
+    c16(int a){
+      this.a=a;
+    }
+    void ss(){
+        System.out.println(a);
+    }
+}
