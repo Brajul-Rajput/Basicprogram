78e81741d95ffc2e58bdcb6e75b3307e999a45e7
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-24 16:36:21 +0530

    Create class42.java

---
+++ src/SHASHWAT/class42.java
@@ -0,0 +1,18 @@
+package SHASHWAT;
+
+public class class42 {
+    static void main() {
+        int arr[]={3,1,3,3,4,7,2,8,4,9,};
+        int target=3;
+        int ans=-1;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i]==target){
+                ans =i;
+                if(true){
+                    break;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
