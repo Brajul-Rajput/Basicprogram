c0557e58429fc6dccc277a36f1737168317542c5
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-09-24 16:36:31 +0530

    Create t2.java

---
+++ src/total451/t2.java
@@ -0,0 +1,12 @@
+package total451;
+
+import static java.lang.Math.sqrt;
+
+public class t2 {
+    static void main() {
+        int a=(int)Math.sqrt(3);
+        for(int i=1;i<a;i++) {
+            System.out.print(i);
+        }
+    }
+}
